ae276f6d842849e6283bc7b51e075e88625dc909
Ernie Shu <ernie.shu@gmail.com>
2018-11-01 13:12:23 -0400

    Added additional Model and DB configuration.

---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public String name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public TodoItem()
+        {
+        }
+    }
+}
